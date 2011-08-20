5924f14abbec6d5e5c66c6b802e6a7059a61f6e9
Roderic Morris <roderyc@gmail.com>
2011-08-20 17:29:27 -0400

    Update calendar settings.

--- static/js/calendar.js
+++ static/js/calendar.js
@@ -1,17 +1,14 @@
 $(document).ready(function() {
   $('#nuacm_calendar').fullCalendar({
-    // US Holidays
-    events: $.fullCalendar.gcalFeed(
-     'http://www.google.com/calendar/feeds/acm@ccs.neu.edu/public/basic',
-      {draggable: false, className: 'mygcal'}
-      ),
-      eventClick: function(event) {
-	window.open(event.url, 'gcalevent', 'width=700,height=600');
-	return false;
-      },
-      loading: function(bool) {
-	if (bool) $('#loading').show();
-	else $('#loading').hide();
-      }
-     });
- });
+    events: "http://www.google.com/calendar/feeds/acm@ccs.neu.edu/public/basic",
+    eventClick: function(event) {
+      window.open(event.url, 'gcalevent', 'width=700,height=600');
+      return false;
+    },
+    loading: function(bool) {
+      if (bool) $('#loading').show();
+      else $('#loading').hide();
+    },
+    header: { left: "today prev,next", center: "title", right: "month,agendaWeek"}
+  });
+});
