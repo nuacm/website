9cf1d2d0029d84923fdd1df32409edb50642edfa
Roderic Morris <roderyc@gmail.com>
2011-09-26 08:30:45 -0400

    Check for paid membership when generating member json. Update member json.

--- generate-site-json.py
+++ generate-site-json.py
@@ -9,7 +9,8 @@ def main(file_name):
   reader = csv.reader(open(file_name))
   # Get rid of header row.
   reader.next()
-  members = [{"name": "%s %s" % (row[1], row[2]), "username": row[4]} for row in reader]
+  members = [{"name": "%s %s" % (row[1], row[2]), "username": row[4]} for row in reader if
+             (len(row) == 10) and row[9] == "Y"]
   json.dump(members, open(os.path.join(SITE_DIR, "media/members.json"), "w"))
 
 if __name__ == "__main__":
