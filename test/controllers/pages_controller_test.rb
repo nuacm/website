a616c3fa485bef4f8f559c5bcf67271bf8a6e093
Nathan Lilienthal <nathan@nixpulvis.com>
2013-04-25 14:57:34 -0400

    renaming main controller to pages

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class MainControllerTest < ActionController::TestCase
+class PagesControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
