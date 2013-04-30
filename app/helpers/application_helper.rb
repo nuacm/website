3a8568ce2b3d35bb9e16ac67a4e03dc426edbc74
Nathan Lilienthal <nathan@nixpulvis.com>
2013-04-30 13:09:02 -0400

    active_link_to added.
    This is a much better method for creating links that detect what page
    the current page is, and then apply the "current" class to the link.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,8 @@
 module ApplicationHelper
 
-  def cp(path)
-    "current" if current_page?(path)
+  def active_link_to(label, path, options = {})
+    options[:class] = "current" if current_page?(path)
+    link_to(label, path, options)
   end
 
 end
