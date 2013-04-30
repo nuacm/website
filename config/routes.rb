06661d68a8c0baadc878817ed524e6d71c993d83
Nathan Lilienthal <nathan@nixpulvis.com>
2013-04-30 12:58:46 -0400

    starting about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 NUACM::Application.routes.draw do
-  get '/',      to: 'pages#home', :as => 'home'
+  get '/',      to: 'pages#home',  :as => 'home'
+  get '/about', to: 'pages#about', :as => 'about'
 end
