eb57a49b6015e2b0354e3dc4b3b0a9e64c4df988
Nathan Lilienthal <nathan@nixpulvis.com>
2013-04-30 13:56:42 -0400

    Use member's email as their unique identifier.
    This will keep things simple, no need for a username.

--- spec/models/member_spec.rb
+++ spec/models/member_spec.rb
@@ -3,5 +3,6 @@ require 'spec_helper'
 describe Member do
   subject { create(:member) }
 
-  it { should validate_presence_of :full_name }
+  it { should validate_presence_of   :full_name }
+  it { should validate_uniqueness_of :email }
 end
