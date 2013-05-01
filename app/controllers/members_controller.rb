0b5f1235feb5b33d096e36388fb9d79a0a936301
Nathan Lilienthal <nathan@nixpulvis.com>
2013-05-01 13:03:19 -0400

    Action for creating a ResetKey for members by email.
    A POST to 'forgot-password' with an email, will look up the
    member by email, and if the member exists, calls forgot_password!
    on that member. This method creates the ResetKey, and will email
    the user with instructions on using it. The page rendered is a
    simple "Email Sent" page.
    
    If the email is not that of a member, then it will redirect back
    and flash an error message.

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,2 +1,13 @@
 class MembersController < ApplicationController
+
+  # POST '/forgot-password'
+  def forgot_password
+    @member = Member.find_by_email(params[:email])
+    if @member
+      @member.forgot_password!
+    else
+      redirect_to :back, :flash => { :error => "No member with email #{params[:email]} found." }
+    end
+  end
+
 end
