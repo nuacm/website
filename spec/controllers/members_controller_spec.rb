056fcd6e65459eb3ffcb5eb54d155f5fdbb59deb
Nathan Lilienthal <nathan@nixpulvis.com>
2013-05-03 23:27:07 -0600

    Test ResetKey create from POST forgot-password.

--- spec/controllers/members_controller_spec.rb
+++ spec/controllers/members_controller_spec.rb
@@ -19,6 +19,16 @@ describe MembersController do
       it "renders the forgot_password template" do
         expect(response).to render_template("forgot_password")
       end
+
+      it "creates a key for the subject" do
+        subject.reset_key.should_not be_nil
+      end
+
+      it "replaces existing keys from the subject" do
+        old_reset_key = subject.reset_key
+        post :forgot_password, :email => subject.email # again
+        subject.reload.reset_key.should_not eq(old_reset_key)
+      end
     end
 
     context "with email that doesn't exist" do
