b6496a5201f6a55f87194366f0532558de822019
Nathan Lilienthal <nathan@nixpulvis.com>
2013-06-15 02:39:14 -0400

    Don't route password reset [:show, :destroy].

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,12 @@ NUACM::Application.routes.draw do
     member { put 'change_password' }
   end
 
+  # Password Resets.
+  resource :password_reset, :except => [:show, :destroy]
+
   # Sessions.
   get    'signup'   => 'members#new',      :as => 'signup'
   get    'login'    => 'sessions#new',     :as => 'login'
   post   'login'    => 'sessions#create',  :as => 'login'
   delete 'logout'   => 'sessions#destroy', :as => 'logout'
-
-  # Password Resets.
-  resource :password_reset
 end
