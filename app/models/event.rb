1b6a0ef8a8bba727173c88e023b59a6e4fc28423
Nathan Lilienthal <nathan@nixpulvis.com>
2013-08-07 18:15:18 -0400

    Event validations (all basic ones)

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,18 @@
+
 class Event < ActiveRecord::Base
+
+  validates :title, :presence => true
+  validates :description, :presence => true
+  validates :location, :presence => true
+  validates :start_time, :presence => true
+  validate :end_time_after_start_time
+
+  private
+
+  def end_time_after_start_time
+    if self.start_time && self.end_time && self.start_time > self.end_time
+      errors.add(:end_time, "must be after start time")
+    end
+  end
+
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe Event do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:description) }
+  it { should validate_presence_of(:location) }
+  it { should validate_presence_of(:start_time) }
+
+  it "validates that start_time is before end_time" do
+    bad_event = Event.new :start_time => DateTime.new(2013, 8, 7, 20, 50),
+                          :end_time => DateTime.new(2013, 8, 7, 18, 30)
+    bad_event.valid?
+    bad_event.errors.should include(:end_time)
+  end
+
 end
