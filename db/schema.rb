7e8e695ccadf23a43315c450f5fcf325c43633d2
Nathan Lilienthal <nathan@nixpulvis.com>
2013-08-07 17:35:49 -0400

    Starting the event model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130619030550) do
+ActiveRecord::Schema.define(version: 20130807213307) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: true do |t|
+    t.string   "title"
+    t.string   "description"
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.string   "location"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "keys", force: true do |t|
     t.string   "token"
