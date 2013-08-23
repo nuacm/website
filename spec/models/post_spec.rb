69588a6f7584b73783a55c9e7539a805e1e4dbfc
Nathan Lilienthal <nathan@nixpulvis.com>
2013-08-22 20:19:23 -0400

    Adding post specs.

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe Post do
-  pending "TODO: write some specs."
+  subject { create(:post) }
+
+  it { should respond_to :title }
+  it { should respond_to :body }
+  it { should respond_to :member }
+
+  it { should validate_presence_of :title }
+  it { should validate_presence_of :body }
+  it { should validate_presence_of :member }
+
+  it "validates that the title doesn't end in a period" do
+    create(:post, :title => "This ends in a period.").should_not be_valid
+  end
+
 end
