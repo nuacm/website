efeac27a6a2e94b5870a3a302d37f0b0e5c381a8
Nathan Lilienthal <nathan@nixpulvis.com>
2013-08-27 14:53:42 -0400

    Posts should always be created as the current member.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,7 +21,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(post_params)
+    @post = current_member.posts.new(post_params)
 
     if @post.save
       redirect_to @post, :notice => "Created post."
