3f6ecf7583eb2d624480b67456313ffa0e15e962
Nathan Lilienthal <nathan@nixpulvis.com>
2013-08-27 04:35:38 -0400

    Get active nav links working again.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,14 @@ module ApplicationHelper
 
   def active_link_to(label, path, options = {})
     options[:class] = "active" if current_page?(path)
-    link_to(label, path, options)
+
+    if options.delete(:li)
+      content_tag :li, options do
+        link_to(label, path)
+      end
+    else
+      link_to(label, path, options)
+    end
   end
 
 end
