6022408ab5c3415dd97ea0d294e00da9dc2523ec
Nathan Lilienthal <nathan@nixpulvis.com>
2013-08-28 02:32:13 -0400

    Basic Mailchimp integration working.

--- app/models/member.rb
+++ app/models/member.rb
@@ -5,6 +5,8 @@ class Member < ActiveRecord::Base
   validates :email, :presence => true, :uniqueness => true, :email => true
 
   before_create { create_authorization_key }
+  after_create { subscribe_to("ACM Members") }
+  after_destroy { unsubscribe_from("ACM Members") }
 
   has_one :authorization_key,
           -> { where :key_type => "authorization_key" },
@@ -24,5 +26,26 @@ class Member < ActiveRecord::Base
   def send_password_reset
     create_password_reset_key(:expires_on => DateTime.current + 2.hours)
     MemberMailer.password_reset(self).deliver
+  end
+
+  # Mailchimp Integration.
+
+  # subscribe_to String ->
+  def subscribe_to(list_name)
+    mailchimp = Mailchimp::API.new(NUACM::Application.config.mailchimp_api_key)
+    list = mailchimp.lists.list['data'].find { |l| l['name'] == list_name }
+    id = list['id']
+    mailchimp.lists.subscribe id, { 'email' => self.email }
+  end
+
+  # unsubscribe_from String ->
+  def unsubscribe_from(list_name)
+    mailchimp = Mailchimp::API.new(NUACM::Application.config.mailchimp_api_key)
+    list = mailchimp.lists.list['data'].find { |l| l['name'] == list_name }
+    id = list['id']
+    begin
+      mailchimp.lists.unsubscribe id, { 'email' => self.email }
+    rescue Mailchimp::EmailNotExistsError
+    end
   end
 end
---
+++ config/initializers/mail_chimp.rb
@@ -0,0 +1,4 @@
+# Be sure to restart your server when you modify this file.
+
+# API Key for our mailchimp account.
+NUACM::Application.config.mailchimp_api_key = ENV['MAILCHIMP_API_KEY']
