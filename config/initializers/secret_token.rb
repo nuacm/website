44bf9f245aa4470e64aac3cbddd84978adaa20be
Nathan Lilienthal <nathan@nixpulvis.com>
2013-10-02 14:22:23 -0400

    Rename production env variable for secret.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -10,7 +10,7 @@
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
 if Rails.env.production?
-  NUACM::Application.config.secret_key_base = ENV['SECRET_TOKEN']
+  NUACM::Application.config.secret_key_base = ENV['SECRET_KEY']
 else
   NUACM::Application.config.secret_key_base = 'e372324fc6a231d1ae223fc2288d9f9cb576fb80a65c6e2fb0721d268979ff4684b349b7e37621e7f47de70fe24618c2dbae054e66255aadb1c5faa1a5ae6e13'
 end
