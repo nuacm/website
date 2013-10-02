d103245b36afe47bd4fcf4846774ac37cb301582
Nathan Lilienthal <nathan@nixpulvis.com>
2013-10-02 15:21:16 -0400

    Don't let production start without mailchimp API.
    fixes #42

--- config/initializers/mail_chimp.rb
+++ config/initializers/mail_chimp.rb
@@ -1,6 +1,11 @@
+# API Key for our mailchimp account.
+#
 # Be sure to restart your server when you modify this file.
 
-# API Key for our mailchimp account.
+if Rails.env.production? && ENV['MAILCHIMP_API_KEY'].nil?
+  raise "ENV['MAILCHIMP_API_KEY'] not set."
+end
+
 Gibbon::API.api_key = ENV['MAILCHIMP_API_KEY']
 Gibbon::API.timeout = 15
 Gibbon::API.throws_exceptions = false
