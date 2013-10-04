f63e52027b1b91106009e6603f3f46a9435749c8
Nathan Lilienthal <nathan@nixpulvis.com>
2013-10-04 09:39:11 -0400

    Updating Rails default files.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-NUACM::Application.config.session_store :cookie_store, key: '_acm_session'
+NUACM::Application.config.session_store :cookie_store, key: '_nuacm_session'
